5d28bc833bd438fd50f9b42c14e2d0322a4a0a80
le3e3p <124032623+le3e3p@users.noreply.github.com>
2023-03-21 12:04:23 +0300

    21.03.2023

---
+++ main (20).py
@@ -0,0 +1,9 @@
+import random
+b = random.randint(10, 99)
+s = str(b)
+n = s[1] + s[0]
+o = int(n)
+print(f"Исходное число: {b}")
+print(f"Новое число: {o}")
+
+
---
+++ main (24).py
@@ -0,0 +1,8 @@
+k=int(input())
+x=int(input())
+if k<x:
+    print(k*x)
+if k>x:
+    print(k+x)
+if k==x:
+    print(k/x)
